ccf7f07ca5eb915fdcdc9973cbc76c9903bd4a97
Sabir <sabiralitanveer6@gmail.com>
2025-09-30 13:49:51 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on PORT: ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("Failed to connect to DB!!!",err);
+});
