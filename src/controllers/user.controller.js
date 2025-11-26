4321575324f2550b005e40b351aba9f3715475a3
Sabir <sabiralitanveer6@gmail.com>
2025-11-26 14:05:51 +0530

    add refresh token endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,8 @@ import {ApiError} from '../utils/ApiError.js';
 import {User} from '../models/user.models.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
+import jwt from 'jsonwebtoken';
+
 
 const generateAccessAndRefreshToken = async(userId)=>{
     try {
@@ -161,7 +163,46 @@ const logoutUser = asyncHandler(async (req, res) => {
     ) 
 });
 
-export {registerUser , loginUser, logoutUser};
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "Refresh token missing");
+    }
+
+    try {
+        const decodedtoken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedtoken?._id)
+        if(!user){
+            throw new ApiError(401, "Invalid refresh token - user not found");
+        }
+    
+        if(incomingRefreshToken !== user?.refreshToken){
+            throw new ApiError(401, "refresh token is expired or used");  
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshToken(user._id)
+    
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(200, {
+                accessToken,
+                refreshToken : newRefreshToken
+            }, "Access token refreshed successfully")
+        )
+    } catch (error) {
+        throw(new ApiError(401, error?.message || "Invalid refresh token"));
+    }
+});
+
+export {registerUser , loginUser, logoutUser , refreshAccessToken};
 // const asyncHandler = require('express-async-handler');
 // const User = require('../models/user.model'); // Adjust the path to your User model
 // const bcrypt = require('bcryptjs');
