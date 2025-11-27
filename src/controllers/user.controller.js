27894a41b2b9006beee7a319b7eb9ed9b2fcd681
Sabir <sabiralitanveer6@gmail.com>
2025-11-27 18:50:52 +0530

    add aggregate pipeline in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,6 @@ import {User} from '../models/user.models.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import jwt from 'jsonwebtoken';
-import { use } from 'react';
 
 
 const generateAccessAndRefreshToken = async(userId)=>{
@@ -288,6 +287,80 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         new ApiResponse(200, user, "User cover image updated successfully")
     )
 })
+
+const getChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+    if(!username.trim()){
+        throw new ApiError(400, "Username is required");
+    }
+    // TODO: complete the aggregation pipeline
+    // TODO channel ko console log karke dekhna h -- what datatype does aggregate return
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                 username: username.toLowerCase() 
+                }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                     $size: "$subscribers" 
+                    },
+                channelSubscribedToCount: { 
+                    $size: "$subscribeTo" 
+                },
+                isSubscribed: {
+                $cond: {
+                    if: {
+                        $in: [req.user?._id, "$subscribers.subscriber"]
+                    },
+                    then: true,
+                    else: false
+                }
+            }
+            },
+            
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+    if(!channel?.length){
+    throw new ApiError(404, "Channel not found");
+    }
+    return res.status(200).json(
+        new ApiResponse(200, channel[0], "Channel profile fetched successfully")
+    )
+
+})
+
+
 export {
     registerUser,
     loginUser,
