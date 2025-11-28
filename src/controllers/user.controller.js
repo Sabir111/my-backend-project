8a14632a1e6796f8f9840782ebae32c9fff4a76f
Sabir <sabiralitanveer6@gmail.com>
2025-11-28 14:40:23 +0530

    add get watchhistory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -360,6 +360,54 @@ const getChannelProfile = asyncHandler(async (req, res) => {
 
 })
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "user",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json(
+        new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully")
+    )
+
+});
 
 export {
     registerUser,
@@ -369,7 +417,10 @@ export {
     changeCurrentPassword,
     getCurrentUser,
     updateAccountDetails,
-    updateUserAvatar
+    updateUserAvatar,
+    updateUserCoverImage,
+    getChannelProfile,
+    getWatchHistory
 };
 // const asyncHandler = require('express-async-handler');
 // const User = require('../models/user.model'); // Adjust the path to your User model
